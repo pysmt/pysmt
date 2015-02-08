82343e3e02a5f4acd421004d0dd2a4876ba3414d
Marco Gario <marco.gario@gmail.com>
2015-02-08 19:36:42 +0100

    moving installation of dependencies withing shippable script

--- shippable.sh
+++ shippable.sh
@@ -2,7 +2,8 @@
 
 if [ "$PYSMT_SOLVER" == "msat" ]
 then
-
+    apt-get install -y make build-essential swig libgmp-dev
+    apt-get install -y python-all-dev
     pushd .
     cd solvers
     ./install.py --confirm-agreement --msat
@@ -12,6 +13,9 @@ fi
 
 if [ "$PYSMT_SOLVER" == "z3" ]
 then
+    apt-get install -y make build-essential swig libgmp-dev
+    apt-get install -y python-all-dev
+
     pushd .
     cd solvers
     ./install.py --confirm-agreement --z3 --make-j 2
