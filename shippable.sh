0c896dcb2aa83783249b0a7dc8c890a57cdbd009
Marco Gario <marco.gario@gmail.com>
2015-02-08 18:24:01 +0100

    Added shippable.sh script

---
+++ shippable.sh
@@ -0,0 +1,16 @@
+if [ "$PYSMT_SOLVER" == "msat" ]
+then
+
+    pushd .
+    cd solvers
+    ./install.py --confirm-agreement --msat
+    popd
+fi
+
+if [ "$PYSMT_SOLVER" == "z3" ]
+then
+    pushd .
+    cd solvers
+    ./install.py --confirm-agreement --z3 --make-j 2
+    popd
+fi
