dc5a6ebf42526659f36ec27e5e2434ca5361537d
MikAnd <micheli.andrea@gmail.com>
2015-02-19 13:51:18 +0100

    Removed Progress Bar, replaced by simple percentege printing

--- install.py
+++ install.py
@@ -21,80 +21,6 @@ import sys
 import platform
 import zipfile
 import argparse
-import sys
-import time
-
-#MG: TODO: Add credits for this class.
-class ProgressBar(object):
-    """ProgressBar class holds the options of the progress bar.
-    The options are:
-        start   State from which start the progress. For example, if start is
-                5 and the end is 10, the progress of this state is 50%
-        end     State in which the progress has terminated.
-        width   --
-        fill    String to use for "filled" used to represent the progress
-        blank   String to use for "filled" used to represent remaining space.
-        format  Format
-        incremental
-    """
-    def __init__(self, start=0, end=10, width=12,
-                 fill='=', blank='.',
-                 format='[%(fill)s>%(blank)s] %(progress)s%%',
-                 incremental=True):
-        super(ProgressBar, self).__init__()
-
-        self.start = start
-        self.end = end
-        self.width = width
-        self.fill = fill
-        self.blank = blank
-        self.format = format
-        self.incremental = incremental
-        self.step = 100 / float(width) #fix
-        self.reset()
-
-    def __add__(self, increment):
-        increment = self._get_progress(increment)
-        if 100 > self.progress + increment:
-            self.progress += increment
-        else:
-            self.progress = 100
-        return self
-
-    def __str__(self):
-        progressed = int(self.progress / self.step) #fix
-        fill = progressed * self.fill
-        blank = (self.width - progressed) * self.blank
-        return self.format % {'fill': fill, 'blank': blank, 'progress': int(self.progress)}
-
-    __repr__ = __str__
-
-    def _get_progress(self, increment):
-        return float(increment * 100) / self.end
-
-    def reset(self):
-        """Resets the current progress to the start point"""
-        self.progress = self._get_progress(self.start)
-        return self
-
-
-class AnimatedProgressBar(ProgressBar):
-    """Extends ProgressBar to allow you to use it straighforward on a script.
-    Accepts an extra keyword argument named `stdout` (by default use sys.stdout)
-    and may be any file-object to which send the progress status.
-    """
-    def __init__(self, *args, **kwargs):
-        super(AnimatedProgressBar, self).__init__(*args, **kwargs)
-        self.stdout = kwargs.get('stdout', sys.stdout)
-
-    def show_progress(self):
-        if hasattr(self.stdout, 'isatty') and self.stdout.isatty():
-            self.stdout.write('\r')
-        else:
-            self.stdout.write('\n')
-        self.stdout.write(str(self))
-        self.stdout.flush()
-
 
 
 CWD = os.path.dirname(os.path.abspath(__file__))
@@ -125,22 +51,26 @@ def download(url, file_name):
     u = urllib2.urlopen(url)
     f = open(file_name, 'wb')
     meta = u.info()
-    bar = None
     if len(meta.getheaders("Content-Length")) > 0:
         file_size = int(meta.getheaders("Content-Length")[0])
         print("Downloading: %s Bytes: %s" % (file_name, file_size))
-        bar = AnimatedProgressBar(end=file_size, width=80)
 
     block_sz = 8192
+    count = 0
     while True:
         buff = u.read(block_sz)
         if not buff:
             break
 
         f.write(buff)
-        if len(meta.getheaders("Content-Length")) > 0:
-            bar + len(buff)
-            bar.show_progress()
+        if len(meta.getheaders("Content-Length")) > 0 and sys.stdout.isatty():
+            count += len(buff)
+            perc = (float(count) / float(file_size)) * 100.0
+            str_perc = "%.1f%%" % perc
+            sys.stdout.write('\r')
+            sys.stdout.write(str_perc)
+            sys.stdout.write(" " * (10 - len(str_perc)))
+
     print ""
     f.close()
 
