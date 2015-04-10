19887525ab551d3cd87d569be302c2df16c0c109
MikAnd <micheli.andrea@gmail.com>
2015-04-10 16:33:17 +0200

    Restored extra option handling for SMT2 wrapper solvers

--- pysmt/smtlib/solver.py
+++ pysmt/smtlib/solver.py
@@ -40,13 +40,18 @@ class SmtLibSolver(Solver):
         # Initialize solver
         self._send_command(SmtLibCommand(smtcmd.SET_OPTION, [":print-success", "false"]))
         self._send_command(SmtLibCommand(smtcmd.SET_OPTION, [":produce-models", "true"]))
-        # if self.options is not None:
-        #     for o,v in iteritems(self.options):
-        #         self._send_command(SmtLibCommand(smtcmd.SET_OPTION, [o, v]))
+
+        if self.options is not None:
+            for o,v in iteritems(self.options):
+                self._send_command(SmtLibCommand(smtcmd.SET_OPTION, [o, v]))
         self._send_command(SmtLibCommand(smtcmd.SET_LOGIC, [logic]))
 
     def get_default_options(self, logic=None, user_options=None):
-        return user_options
+        res = {}
+        for o,v in iteritems(user_options):
+            if o not in ["generate_models", "unsat_cores_mode"]:
+                res[o] = v
+        return res
 
     def _send_command(self, cmd):
         if self.dbg: print("Sending: " + cmd.serialize_to_string())
