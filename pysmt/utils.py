99f7c01beb847f2f3bba6f5b7d49c8ae0641f0ab
MikAnd <micheli.andrea@gmail.com>
2015-05-06 15:29:38 +0200

    FNODE: Extended support to infix notation as per #92.

--- pysmt/utils.py
+++ pysmt/utils.py
@@ -1,11 +1,20 @@
 import io
 from six import PY2
+from fractions import Fraction
 
-def is_integer(n):
+def is_python_integer(n):
     if PY2:
         return type(n) == long or type(n) == int
     else:
         return type(n) == int
+
+
+def is_python_rational(n):
+    return is_python_integer(n) or type(n) == float or type(n) == Fraction
+
+
+def is_python_boolean(n):
+    return n is True or n is False
 
 
 def BufferedTextReader(fname):
