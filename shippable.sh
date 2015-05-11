0472dfb260d85a206a5b2400d101c9101877bb3e
MikAnd <micheli.andrea@gmail.com>
2015-05-11 14:07:34 +0200

    CI: Enabled coverage test with all the solvers in shippable
    Now the tests comprehend a new setting in which all the solvers are
    contemporary enabled and covergae anaysis is enabled.

--- shippable.sh
+++ shippable.sh
@@ -1,31 +1,29 @@
-if [ "$1" == "msat" ]
+apt-get update
+
+if [ "$1" == "msat" ] || [ "$1" == "all" ]
 then
-    apt-get update
     apt-get install -y make build-essential swig libgmp-dev
     apt-get install -y python-all-dev
     ./install.py --confirm-agreement --msat
 fi
 
-if [ "$1" == "msat_wrap" ]
+if [ "$1" == "msat_wrap" ] || [ "$1" == "all" ]
 then
-    apt-get update
     apt-get install -y make build-essential swig libgmp-dev
     apt-get install -y python-all-dev
     ./install.py --confirm-agreement --msat
 fi
 
 
-if [ "$1" == "z3" ]
+if [ "$1" == "z3" ] || [ "$1" == "all" ]
 then
-    apt-get update
     apt-get install -y make build-essential swig libgmp-dev
     apt-get install -y python-all-dev
     ./install.py --confirm-agreement --z3 --make-j 2
 fi
 
-if [ "$1" == "cvc4" ]
+if [ "$1" == "cvc4" ] || [ "$1" == "all" ]
 then
-    apt-get update
     apt-get install -y make build-essential swig libgmp-dev \
                        python-all-dev \
                        autoconf libtool antlr3 wget \
@@ -34,25 +32,23 @@ then
 fi
 
 
-if [ "$1" == "yices" ]
+if [ "$1" == "yices" ] || [ "$1" == "all" ]
 then
     easy_install ctypesgen
     ./install.py --confirm-agreement --yices
 fi
 
 
-if [ "$1" == "cudd" ]
+if [ "$1" == "cudd" ] || [ "$1" == "all" ]
 then
-    apt-get update
     apt-get install -y make build-essential swig
     apt-get install -y python-all-dev
     ./install.py --confirm-agreement --cudd  --make-j 2
 fi
 
 
-if [ "$1" == "picosat" ]
+if [ "$1" == "picosat" ] || [ "$1" == "all" ]
 then
-    apt-get update
     apt-get install -y make build-essential swig
     apt-get install -y python-all-dev
     ./install.py --confirm-agreement --picosat
