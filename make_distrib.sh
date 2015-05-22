258f957494e950de52247190266e695cb0aabdba
Marco Gario <marco.gario@gmail.com>
2015-05-22 09:35:01 +0200

    Packaging: Added information for Wheel creation

--- make_distrib.sh
+++ make_distrib.sh
@@ -3,3 +3,10 @@
 python setup.py bdist --format=gztar
 python setup.py sdist --format=gztar
 
+# Wheel file
+python setup.py bdist_wheel --universal
+
+wget https://bitbucket.org/gutworth/six/raw/8a545f4e906f6f479a6eb8837f31d03731597687/six.py -O dist/six.py
+echo "To create a self-contained wheel pkg, manually add six:"
+echo " $ zip PySMT-version.whl six.py"
+echo "A copy of six.py has been downloaded in dist/"
