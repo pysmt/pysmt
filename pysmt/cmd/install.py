8cfe8beeee59ae512ba7653e9e43de0d215bc1dd
Marco Gario <marco.gario@gmail.com>
2015-06-14 11:28:19 +0200

    CI: Update MathSAT to 5.3.6

--- pysmt/cmd/install.py
+++ pysmt/cmd/install.py
@@ -156,7 +156,7 @@ def download(url, file_name):
 def install_msat(options):
     """Installer for the MathSAT5 solver python interafce"""
 
-    base_name =  "mathsat-5.3.5-linux-%s" % get_architecture()
+    base_name =  "mathsat-5.3.6-linux-%s" % get_architecture()
     archive_name = "%s.tar.gz" % base_name
     archive = os.path.join(BASE_DIR, archive_name)
     dir_path = os.path.join(BASE_DIR, base_name)
