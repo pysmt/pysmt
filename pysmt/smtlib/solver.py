5516867ffd35c7fd80944f50f290d312c452d645
Marco Gario <marco.gario@gmail.com>
2016-08-14 11:39:00 +0200

    BUGFIX: Add delay after starting Wrapper to detect dead process
    If the process of the generic SMT-Wrapper did not start yet, the write
    on the pipe is queued. If the process dies (or never starts) this does
    not result in an IOError, but it looks like an empty read from the pipe.
    To avoid this, we way 0.01s after creating the process, to make sure
    that the write will either raise an IOError or be successful.

--- pysmt/smtlib/solver.py
+++ pysmt/smtlib/solver.py
@@ -1,3 +1,18 @@
+# Copyright 2014 Andrea Micheli and Marco Gario
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import time
 from io import TextIOWrapper
 from subprocess import Popen, PIPE
 
@@ -30,6 +45,8 @@ class SmtLibSolver(Solver):
         self.args = args
         self.declared_vars = set()
         self.solver = Popen(args, stdout=PIPE, stderr=PIPE, stdin=PIPE)
+        # Give time to the process to start-up
+        time.sleep(0.01)
         self.parser = SmtLibParser(interactive=True)
         if PY2:
             self.solver_stdin = self.solver.stdin
