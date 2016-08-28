11f93321e321f81619bbb2ac1c3cbf0c7e603a75
Marco Gario <marco.gario@gmail.com>
2016-08-28 10:12:31 +0200

    CI: Skip testing on regular push for py2.7 or pypy
    Created travis_install.sh and travis_script.sh to run the install and
    script phase

---
+++ ci/travis_install.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+set -ev
+
+#
+# Skip Install if Python 2.7 or PyPy and not a PR
+#
+if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
+    echo "Regular Push (Not-PR):"
+    if [ "${TRAVIS_PYTHON_VERSION}" == "2.7" ]; then
+        echo "Skipping Python 2.7"
+        exit 0
+    fi
+    if [ "${TRAVIS_PYTHON_VERSION}" == "pypy" ]; then
+        echo "Skipping Python PyPy"
+        exit 0
+    fi
+fi
+
+
+# This is for btor that fails to find the python 3 library to link
+export LIBRARY_PATH=${LIBRARY_PATH}:/opt/python/3.5.0/lib
+
+# For some reason, Travis CI cannot find the command python3.5-config.
+# Therefore, we force the path here
+if [ "${TRAVIS_PYTHON_VERSION}" == "3.5" ];
+then
+    export PATH=${PATH}:/opt/python/3.5.0/bin/ ;
+fi
+
+pip install six
+
+if [ "${PYSMT_SOLVER}" == "all" ] || [ "${PYSMT_SOLVER}" == "btor" ];
+then
+    pip install cython;
+fi
+
+#
+# Install Solvers
+#
+export BINDINGS_FOLDER=${HOME}/python_bindings/${PYSMT_SOLVER}
+mkdir -p ${BINDINGS_FOLDER}
+python install.py --confirm-agreement --bindings-path ${BINDINGS_FOLDER}
+eval `python install.py --env --bindings-path ${BINDINGS_FOLDER}`
+
+if [ "${PYSMT_SOLVER}" == "all" ] || [ "${PYSMT_SOLVER}" == "z3_wrap" ]; then
+    python install.py --z3 --conf --force;
+    cp -v $(find ~/.smt_solvers/ -name z3 -type f) pysmt/test/smtlib/bin/z3;
+    chmod +x pysmt/test/smtlib/bin/z3;
+    mv pysmt/test/smtlib/bin/z3.solver.sh.template pysmt/test/smtlib/bin/z3.solver.sh ;
+fi
+
+if [ "${PYSMT_SOLVER}" == "all" ] || [ "${PYSMT_SOLVER}" == "msat_wrap" ];
+then
+    python install.py --msat --conf --force;
+    cp -v $(find ~/.smt_solvers/ -name mathsat -type f) pysmt/test/smtlib/bin/mathsat;
+    mv pysmt/test/smtlib/bin/mathsat.solver.sh.template pysmt/test/smtlib/bin/mathsat.solver.sh ;
+fi
---
+++ ci/travis_script.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -ev
+
+
+#
+# Skip Install if Python 2.7 or PyPy and not a PR
+#
+if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
+    echo "Regular Push (Not-PR):"
+    if [ "${TRAVIS_PYTHON_VERSION}" == "2.7" ]; then
+        echo "Skipping Python 2.7"
+        exit 0
+    fi
+
+    if [ "${TRAVIS_PYTHON_VERSION}" == "pypy" ]; then
+        echo "Skipping Python PyPy"
+        exit 0
+    fi
+fi
+
+export BINDINGS_FOLDER=${HOME}/python_bindings/${PYSMT_SOLVER}
+eval `python install.py --env --bindings-path ${BINDINGS_FOLDER}`
+echo ${PYTHONPATH}
+python install.py --check
+
+#
+# Run the test suite
+#
+nosetests pysmt -v
+
+#
+# Test examples in examples/ folder
+#
+if [ "${PYSMT_SOLVER}" == "all" ];
+then
+    python install.py --msat --conf --force;
+    cp -v $(find ~/.smt_solvers/ -name mathsat -type f) /tmp/mathsat;
+    (for ex in `ls examples/*.py`; do echo $ex; python $ex || exit $?; done);
+fi
