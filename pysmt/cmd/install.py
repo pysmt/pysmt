696ccba6e6f19ba75a1c3124d19ad78d2a505336
Marco Gario <marco.gario@gmail.com>
2017-06-22 19:23:21 -0400

    Upgrade MSAT to 5.4.1

--- pysmt/cmd/install.py
+++ pysmt/cmd/install.py
@@ -31,7 +31,7 @@ from pysmt import git_version
 
 # Build a list of installers, one for each solver
 Installer = namedtuple("Installer", ["InstallerClass", "version", "extra_params"])
-INSTALLERS = [Installer(MSatInstaller,    "5.3.13", {}),
+INSTALLERS = [Installer(MSatInstaller,    "5.4.1", {}),
               Installer(Z3Installer,      "4.4.1", {"osx": "10.11"}),
               Installer(CVC4Installer,    "1.5-prerelease", {"git_version" : "c15ff43597b41ea457befecb1b0e2402e28cb523"}),
               Installer(YicesInstaller,   "2.5.1", {"yicespy_version": "07439670a54d08a76cfb931194e1eaf07ea026a1"}),
