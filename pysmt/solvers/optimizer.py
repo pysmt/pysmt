7007e8545a48d85c3179100654dde4a7d95c1be6
MikAnd <micheli.andrea@gmail.com>
2018-01-23 15:11:51 +0100

    Finished factorization of out-of-solver optimizers

--- pysmt/solvers/optimizer.py
+++ pysmt/solvers/optimizer.py
@@ -78,95 +78,126 @@ class Optimizer(Solver):
             raise PysmtValueError("Invalid optimization function type: %s" % otype)
 
 
-
 class ExternalOptimizerMixin(Optimizer):
     """An optimizer that uses an SMT-Solver externally"""
 
     def _setup(self):
         raise NotImplementedError
 
-    def _cleanup(self):
+    def _cleanup(self, client_data):
         raise NotImplementedError
 
-    def _check_improve(self, cost_function, cost_so_far, lt):
+    def _pareto_setup(self, client_data):
+        raise NotImplementedError
+
+    def _pareto_cleanup(self, client_data):
+        raise NotImplementedError
+
+    def _pareto_check_progress(self, client_data, cost_functions,
+                               costs_so_far, lts, les):
+        raise NotImplementedError
+
+    def _pareto_block_model(self, client_data, cost_functions, last_model, lts, les):
+        raise NotImplementedError
+
+    def _optimization_check_progress(self, client_data, cost_function,
+                                     cost_so_far, lt, le):
         raise NotImplementedError
 
     def optimize(self, cost_function):
         last_model = None
         optimum_found = False
 
-        self._setup()
-
-        lt, _ = self._comparation_functions(cost_function)
+        lt, le = self._comparation_functions(cost_function)
 
         cost_so_far = None
+        client_data = self._setup()
         while not optimum_found:
             if cost_so_far is None:
                 optimum_found = not self.solve()
             else:
-                optimum_found = self._check_improve(cost_function, cost_so_far, lt)
+                optimum_found = self._optimization_check_progress(client_data,
+                                                                  cost_function,
+                                                                  cost_so_far,
+                                                                  lt, le)
 
             if not optimum_found:
                 last_model = self.get_model()
                 cost_so_far = self.get_value(cost_function)
 
-        self._cleanup()
+        self._cleanup(client_data)
         return last_model
+
+    def pareto_optimize(self, cost_functions):
+        lts, les = zip(*(self._comparation_functions(x) for x in cost_functions))
+
+        terminated = False
+        client_data = self._setup()
+        while not terminated:
+            last_model = None
+            optimum_found = False
+            costs_so_far = None
+            self._pareto_setup(client_data)
+            while not optimum_found:
+                optimum_found = self._pareto_check_progress(client_data,
+                                                            cost_functions,
+                                                            costs_so_far,
+                                                            lts, les)
+                if not optimum_found:
+                    last_model = self.get_model()
+                    costs_so_far = []
+                    for i in range(len(cost_functions)):
+                        costs_so_far.append(self.get_value(cost_functions[i]))
+            self._pareto_cleanup(client_data)
+            if last_model is not None:
+                yield last_model
+                self._pareto_block_model(client_data, cost_functions,
+                                         last_model, lts, les)
+            else:
+                terminated = True
+        self._cleanup(client_data)
 
 
 class SUAOptimizerMixin(ExternalOptimizerMixin):
     """Optimizer mixin using solving under assumptions"""
 
     def _setup(self):
+        return []
+
+    def _cleanup(self, client_data):
         pass
 
-    def _cleanup(self):
-        pass
-
-    def _check_improve(self, cost_function, cost_so_far, lt):
+    def _optimization_check_progress(self, client_data, cost_function,
+                                     cost_so_far, lt, le):
         k = lt(cost_function, cost_so_far)
         return not self.solve(assumptions=[k])
 
-    def pareto_optimize(self, cost_functions):
+    def _pareto_setup(self, client_data):
+        pass
+
+    def _pareto_cleanup(self, client_data):
+        pass
+
+    def _pareto_check_progress(self, client_data, cost_functions,
+                               costs_so_far, lts, les):
         mgr = self.environment.formula_manager
+        if costs_so_far is not None:
+            k = [les[i](cost_functions[i], costs_so_far[i])
+                 for i in range(len(cost_functions))]
+            k.append(mgr.Or(lts[i](cost_functions[i], costs_so_far[i])
+                            for i in range(len(cost_functions))))
+            return not self.solve(assumptions=client_data + k)
+        else:
+            return not self.solve(assumptions=client_data)
 
-        lts, les = zip(*(self._comparation_functions(x) for x in cost_functions))
-
-        eliminated = []
-        terminated = False
-        while not terminated:
-            last_model = None
-            optimum_found = False
-            costs_so_far = None
-            while not optimum_found:
-                if costs_so_far is not None:
-                    k = [les[i](cost_functions[i], costs_so_far[i])
-                         for i in range(len(cost_functions))]
-                    k.append(mgr.Or(lts[i](cost_functions[i], costs_so_far[i])
-                                    for i in range(len(cost_functions))))
-                    optimum_found = not self.solve(assumptions=eliminated + k)
-                else:
-                    optimum_found = not self.solve(assumptions=eliminated)
-
-                if not optimum_found:
-                    last_model = self.get_model()
-                    costs_so_far = []
-                    for i in range(len(cost_functions)):
-                        costs_so_far.append(self.get_value(cost_functions[i]))
-            if last_model is not None:
-                yield last_model
-                eliminated.append(mgr.Or(lts[i](cost_functions[i],
-                                                  last_model[cost_functions[i]])
-                                         for i in range(len(cost_functions))))
-            else:
-                terminated = True
+    def _pareto_block_model(self, client_data, cost_functions, last_model, lts, les):
+        mgr = self.environment.formula_manager
+        client_data.append(mgr.Or(lts[i](cost_functions[i],
+                                         last_model[cost_functions[i]])
+                                  for i in range(len(cost_functions))))
 
     def can_diverge_for_unbounded_cases(self):
         return True
-
-
-
-
 
 
 class IncrementalOptimizerMixin(ExternalOptimizerMixin):
@@ -174,51 +205,39 @@ class IncrementalOptimizerMixin(ExternalOptimizerMixin):
 
     def _setup(self):
         self.push()
+        return None
 
-    def _cleanup(self):
+    def _cleanup(self, client_data):
         self.pop()
 
-    def _check_improve(self, cost_function, cost_so_far, lt):
+    def _optimization_check_progress(self, client_data, cost_function,
+                                     cost_so_far, lt, le):
         self.add_assertion(lt(cost_function, cost_so_far))
         return not self.solve()
 
-    def pareto_optimize(self, cost_functions):
-        mgr = self.environment.formula_manager
-
-        lts, les = zip(*(self._comparation_functions(x) for x in cost_functions))
-
-        terminated = False
+    def _pareto_setup(self, client_data):
         self.push()
-        while not terminated:
-            last_model = None
-            optimum_found = False
-            costs_so_far = None
-            self.push()
-            while not optimum_found:
-                if costs_so_far is not None:
-                    for i in range(len(cost_functions)):
-                        self.add_assertion(les[i](cost_functions[i],
-                                                  costs_so_far[i]))
-                    self.add_assertion(mgr.Or(lts[i](cost_functions[i],
-                                                     costs_so_far[i])
-                                              for i in range(len(cost_functions))))
-                optimum_found = not self.solve()
 
-                if not optimum_found:
-                    last_model = self.get_model()
-                    costs_so_far = []
-                    for i in range(len(cost_functions)):
-                        costs_so_far.append(self.get_value(cost_functions[i]))
-
-            self.pop()
-            if last_model is not None:
-                yield last_model
-                self.add_assertion(mgr.Or(lts[i](cost_functions[i],
-                                                   last_model[cost_functions[i]])
-                                          for i in range(len(cost_functions))))
-            else:
-                terminated = True
+    def _pareto_cleanup(self, client_data):
         self.pop()
+
+    def _pareto_check_progress(self, client_data, cost_functions,
+                               costs_so_far, lts, les):
+        mgr = self.environment.formula_manager
+        if costs_so_far is not None:
+            for i in range(len(cost_functions)):
+                self.add_assertion(les[i](cost_functions[i],
+                                          costs_so_far[i]))
+            self.add_assertion(mgr.Or(lts[i](cost_functions[i],
+                                             costs_so_far[i])
+                                      for i in range(len(cost_functions))))
+        return not self.solve()
+
+    def _pareto_block_model(self, client_data, cost_functions, last_model, lts, les):
+        mgr = self.environment.formula_manager
+        self.add_assertion(mgr.Or(lts[i](cost_functions[i],
+                                         last_model[cost_functions[i]])
+                                  for i in range(len(cost_functions))))
 
     def can_diverge_for_unbounded_cases(self):
         return True
