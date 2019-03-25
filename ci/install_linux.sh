014a436634ae9b2c794ac63ae72954b32ef1c025
MikAnd <micheli.andrea@gmail.com>
2019-03-25 16:39:07 +0100

    Trying to add pypy

--- ci/install_linux.sh
+++ ci/install_linux.sh
@@ -2,10 +2,16 @@
 set -ev
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PIP_INSTALL="python -m pip install --upgrade"
+
+PYTHON="python"
+if [ "${PYTHON_VERSION}" == "pypy" ] || [ "${PYTHON_VERSION}" == "pypy3" ]
+then
+    PYTHON="${PYTHON_VERSION}"
+fi
+PIP_INSTALL="${PYTHON} -m pip install --upgrade"
 
 # Check that the correct version of Python is running.
-python ${DIR}/check_python_version.py "${PYTHON_VERSION}"
+${PYTHON} ${DIR}/check_python_version.py "${PYTHON_VERSION}"
 
 # Install latest version of SWIG for CVC4
 # (The other solvers in isolation fall-back to the system swig)
@@ -51,34 +57,34 @@ fi
 
 
 # Install the solver(s)!
-python install.py --confirm-agreement
+${PYTHON} install.py --confirm-agreement
 
 # Install the binaries for the *_wrap case
 if [ "${PYSMT_SOLVER}" == "all" ] || [ "${PYSMT_SOLVER}" == *"z3_wrap"* ]; then
-    python install.py --z3 --conf --force;
+    ${PYTHON} install.py --z3 --conf --force;
     cp -v $(find ~/.smt_solvers/ -name z3 -type f) pysmt/test/smtlib/bin/z3;
     chmod +x pysmt/test/smtlib/bin/z3;
     mv pysmt/test/smtlib/bin/z3.solver.sh.template pysmt/test/smtlib/bin/z3.solver.sh ;
 fi
 if [ "${PYSMT_SOLVER}" == "all" ] || [ "${PYSMT_SOLVER}" == *"msat_wrap"* ];
 then
-    python install.py --msat --conf --force;
+    ${PYTHON} install.py --msat --conf --force;
     cp -v $(find ~/.smt_solvers/ -name mathsat -type f) pysmt/test/smtlib/bin/mathsat;
     mv pysmt/test/smtlib/bin/mathsat.solver.sh.template pysmt/test/smtlib/bin/mathsat.solver.sh ;
 fi
 
 # Check that the solvers are installed
-python install.py --check
+${PYTHON} install.py --check
 
 
 # Run the test suite
-python -m nose pysmt -v # --with-coverage --cover-package=pysmt
+${PYTHON} -m nose pysmt -v # --with-coverage --cover-package=pysmt
 
 
 # Test examples in examples/ folder
 if [ "${PYSMT_SOLVER}" == "all" ];
 then
-    python install.py --msat --conf --force;
+    ${PYTHON} install.py --msat --conf --force;
     cp -v $(find ~/.smt_solvers/ -name mathsat -type f) /tmp/mathsat;
 
     # since we're relying on relative `pysmt` import in examples,
@@ -87,6 +93,6 @@ then
 
     for ex in examples/*.py; do
         echo $ex
-        python $ex
+        ${PYTHON} $ex
     done
 fi
