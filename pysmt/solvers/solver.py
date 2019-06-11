025c0ad54b06dc70b97e76c3e30a44f6f1a536b5
Makai Mann <makaim@stanford.edu>
2019-06-11 15:31:49 -0700

    Move _check_unsat_core_config to UnsatCoreSolver

--- pysmt/solvers/solver.py
+++ pysmt/solvers/solver.py
@@ -406,6 +406,19 @@ class UnsatCoreSolver(object):
 
     UNSAT_CORE_SUPPORT = True
 
+    def _check_unsat_core_config(self):
+        if self.options.unsat_cores_mode is None:
+            raise SolverNotConfiguredForUnsatCoresError
+
+        if self.last_result is None or self.last_result:
+            raise SolverStatusError("The last call to solve() was not" \
+                                    " unsatisfiable")
+
+        if self.last_command != "solve":
+            raise SolverStatusError("The solver status has been modified by a" \
+                                    " '%s' command after the last call to" \
+                                    " solve()" % self.last_command)
+
     def get_unsat_core(self):
         """Returns the unsat core as a set of formulae.
 
