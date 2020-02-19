4f4d42585b0cf270dbd5cfc2f49492ef90cefbd2
PatrickTrentin88 <patrick.trentin.88@gmail.com>
2020-02-19 16:08:45 +0100

    Modified OptiMathSAT installation script to download the most recent (unofficial) version of OptiMathSAT

--- pysmt/cmd/installers/optimsat.py
+++ pysmt/cmd/installers/optimsat.py
@@ -37,7 +37,9 @@ class OptiMSatInstaller(SolverInstaller):
         archive_name = "optimathsat-%s-%s-%s.%s" % (solver_version, os_name,
                                                     arch, ext)
 
-        native_link = "http://optimathsat.disi.unitn.it/releases/optimathsat-%s/{archive_name}" % solver_version
+        # FIXME(PT): restore original link
+        #native_link = "http://optimathsat.disi.unitn.it/releases/optimathsat-%s/{archive_name}" % solver_version
+        native_link = "http://disi.unitn.it/trentin/resources/optimathsat-%s/{archive_name}" % solver_version
 
         SolverInstaller.__init__(self, install_dir=install_dir,
                                  bindings_dir=bindings_dir,
