95f89480182a5f8cef56a73c9c4f1ec1234befc5
Francesco Arrighi <arrighi.079@gmail.com>
2020-04-03 04:57:11 +0200

    spostato il file pysmt/solvers/optimizer.py in /pysmt/optimization/optimizer.py

--- pysmt/solvers/yices.py
+++ pysmt/solvers/yices.py
@@ -31,7 +31,7 @@ except ImportError:
 from pysmt.solvers.eager import EagerModel
 from pysmt.solvers.solver import Solver, Converter, SolverOptions
 from pysmt.solvers.smtlib import SmtLibBasicSolver, SmtLibIgnoreMixin
-from pysmt.solvers.optimizer import SUAOptimizerMixin, IncrementalOptimizerMixin
+from pysmt.optimization.optimizer import SUAOptimizerMixin, IncrementalOptimizerMixin
 
 from pysmt.walkers import DagWalker
 from pysmt.exceptions import SolverReturnedUnknownResultError
