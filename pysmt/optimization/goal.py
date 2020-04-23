9705853903a3ea367a81891a617259fb247cccc5
Francesco Arrighi <arrighi.079@gmail.com>
2020-04-23 17:00:51 +0200

    added header and fixed errors in class MaxSMT

--- pysmt/optimization/goal.py
+++ pysmt/optimization/goal.py
@@ -1,16 +1,33 @@
+#
+# This file is part of pySMT.
+#
+#   Copyright 2014 Andrea Micheli and Marco Gario
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+
 from pysmt.shortcuts import Times, Int
 
-"""
-Abstract class for goals
-"""
 class Goal:
     pass
 
-"""
-Maximization goal common to all solvers.
-Attention: some solvers may not support this goal
-"""
 class MaximizationGoal(Goal):
+    """
+    Maximization goal common to all solvers.
+    The object can be passed as an argument to the optimize method of any Optimizer
+    Attention: some Optimizer may not support this goal
+    """
+
     def __init__(self, formula):
         self.formula = formula
 
@@ -20,11 +37,13 @@ class MaximizationGoal(Goal):
     def minimize_term(self):
         return Times(Int(-1), self.formula)
 
-"""
-Minimization goal common to all solvers.
-Attention: some solvers may not support this goal
-"""
+
 class MinimizationGoal(Goal):
+    """
+    Minimization goal common to all solvers.
+    The object can be passed as an argument to the optimize method of any Optimizer
+    Attention: some Optimizer may not support this goal
+    """
     def __init__(self, formula):
         self.formula = formula
 
@@ -34,18 +53,17 @@ class MinimizationGoal(Goal):
     def minimize_term(self):
         return self.formula
 
-"""
-MaxSMT goal common to all solvers.
-Attention: some solvers may not support this goal
-"""
+
 class MaxSMT(Goal):
+    """
+    MaxSMT goal common to all solvers.
+    Attention: some solvers may not support this goal
+    """
 
-    """Accepts soft clauses and the relative weights"""
     def __init__(self, clause, weight):
-        self.soft = []
-        for x in range(min(len(clause), len(weight))):
-            self.soft.append((clause.pop(0), weight.pop(0)))
+        """Accepts soft clauses and the relative weights"""
+        self.soft =  zip(clause, weight)
 
-    """Accepts soft clauses and the relative weights"""
     def add_soft_clause(self, clause, weight):
-        self.soft.append((clause.pop(0), weight.pop(0)))
+        """Accepts soft clauses and the relative weights"""
+        self.soft.append((clause, weight))
