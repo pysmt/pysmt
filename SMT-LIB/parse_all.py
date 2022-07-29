dd120ea755e899ecc952349fba5da96702f9a423
Enrico Magnago <en.magnago@gmail.com>
2022-07-29 20:52:35 +0100

    keep parsing even if some benchmarks cause exceptions. use timeit instead of manually computing the difference between the timestamps. allow multiple reruns of each benchmark.

--- SMT-LIB/parse_all.py
+++ SMT-LIB/parse_all.py
@@ -1,10 +1,10 @@
 import os
-import time
+from timeit import timeit
 import random
 import multiprocessing
 import argparse
 
-from itertools import islice
+from itertools import islice, repeat
 
 from pysmt.shortcuts import reset_env, read_smtlib
 
@@ -20,24 +20,26 @@ def get_all_smt_files(target_dir=None):
                 yield os.path.join(root, f)
 
 
-def execute_script_fname(smtfile):
+def execute_script_fname(smtfile, reps):
     """Read and call a Solver to solve the instance"""
     print(smtfile)
     reset_env()
     assert os.path.exists(smtfile)
-    start = time.time()
-    read_smtlib(smtfile, False)
-    end = time.time()
-    return ((end - start), smtfile)
+    assert reps > 0
+    try:
+        exec_time = timeit(lambda: read_smtlib(smtfile, False), number=reps)
+    except:
+        exec_time = -1
+    return (exec_time, smtfile)
 
 
 def dump_stats(timings, fname):
     if fname is None:
         fname = "stats.out"
-    with open(fname, "w") as f:
+    with open(fname, mode='w', encoding="utf-8") as f:
         f.write('filename, time\n')
-        for k in timings:
-            f.write('%f, "%s"\n' % k)
+        for val, bench_name in timings:
+            f.write(f'"{bench_name}", {val}\n')
 
 
 def main():
@@ -52,6 +54,9 @@ def main():
     parser.add_argument('--out', type=str, default="stats.out", nargs='?',
                         help='Where to save the statistics')
 
+    parser.add_argument('--reps', type=int, default=1,
+                        help="number of times each benchmark is run")
+
     args = parser.parse_args()
 
     random.seed(42)
@@ -63,17 +68,25 @@ def main():
     else:
         files_cnt = args.count
 
+    timings = []
+    errors = []
+
     with multiprocessing.Pool() as p:
-        print("Submitting %d jobs, %d at the time" % (files_cnt, chunks))
-        timings = p.map(execute_script_fname, islice(file_list, files_cnt), chunks)
+        print(f"Submitting {files_cnt} jobs, {chunks} at the time")
+        for el in p.starmap(execute_script_fname,
+                            zip(islice(file_list, files_cnt),
+                                repeat(args.reps)),
+                            chunks):
+            (timings if el[0] > 0 else errors).append(el)
 
     mean = sum(x[0] for x in timings) / len(timings)
-    print("The mean execution time was %0.2f seconds" % mean)
-    print("The max execution time was %0.2f seconds" % max(x[0] for x in timings))
+    print(f"The mean execution time was {mean:.2f} seconds")
+    print(f"The max execution time was {max(x[0] for x in timings):.2f} seconds")
+    print(f"Found {len(errors)} errors: {[el[1] for el in errors]}")
 
     outfile = args.out
     dump_stats(timings, outfile)
-    print("The statistics file has been generated in '%s'" % outfile)
+    print(f"The statistics file has been generated in '{outfile}'")
 
 
 if __name__ == '__main__':
