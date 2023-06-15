ab795a90810271763ba6a4f5ebedde2ce897cc17
masinag <gabriele.masina@virgilio.it>
2023-06-15 21:51:44 +0200

    Use MathSAT5Solver.all_sat method in allsat and allsmt examples

--- examples/allsat.py
+++ examples/allsat.py
@@ -1,15 +1,10 @@
 # This example requires MathSAT to be installed.
-#
-# This example shows how to use a solver converter to access
-# functionalities of the solver that are not wrapped by pySMT.
-#
-# Our goal is to call the method msat_all_sat from the MathSAT API.
-#
-import mathsat
-from pysmt.shortcuts import Or, Symbol, Solver, And
+
+from pysmt.shortcuts import And, Or, Solver, Symbol
+
 
 def callback(model, converter, result):
-    """Callback for msat_all_sat.
+    """Callback for msat.all_sat.
 
     This function is called by the MathSAT API everytime a new model
     is found. If the function returns 1, the search continues,
@@ -19,21 +14,17 @@ def callback(model, converter, result):
     # Converter.back() provides the pySMT representation of a solver term.
     py_model = [converter.back(v) for v in model]
     result.append(And(py_model))
-    return 1 # go on
+    return 1  # go on
+
 
 x, y = Symbol("x"), Symbol("y")
 f = Or(x, y)
 
+result = []
 with Solver(name="msat") as msat:
-    converter = msat.converter # .converter is a property implemented by all solvers
-    msat.add_assertion(f) # This is still at pySMT level
+    converter = msat.converter  # .converter is a property implemented by all solvers
+    msat.add_assertion(f)
+    msat.all_sat([x], lambda model: callback(model, converter, result))
 
-    result = []
-    # Directly invoke the mathsat API !!!
-    # The second term is a list of "important variables"
-    mathsat.msat_all_sat(msat.msat_env(),
-        [converter.convert(x)],      # Convert the pySMT term into a MathSAT term
-        lambda model : callback(model, converter, result))
-
-    print("'exists y . %s' is equivalent to '%s'" %(f, Or(result)))
-    #exists y . (x | y) is equivalent to ((! x) | x)
+    print("'exists y . %s' is equivalent to '%s'" % (f, Or(result)))
+    # exists y . (x | y) is equivalent to ((! x) | x)
