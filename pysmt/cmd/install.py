f0b77611de83f2496d40ef4060438e1614bf3ab5
MikAnd <micheli.andrea@gmail.com>
2023-07-26 16:45:35 +0200

    Bumped version of some solvers

--- pysmt/cmd/install.py
+++ pysmt/cmd/install.py
@@ -32,13 +32,13 @@ from pysmt import __version__ as pysmt_version
 Installer = namedtuple("Installer",
                        ["InstallerClass", "version", "extra_params"])
 INSTALLERS = [
-    Installer(MSatInstaller,    "5.6.7", {}),
+    Installer(MSatInstaller,    "5.6.10", {}),
     Installer(CVC4Installer,    "1.7-prerelease",
               {"git_version" : "391ab9df6c3fd9a3771864900c1718534c1e4666"}),
-    Installer(Z3Installer,      "4.8.17", {"osx": "10.16"}),
+    Installer(Z3Installer,      "4.12.2", {"osx": "10.16"}),
     Installer(YicesInstaller,   "2.6.2",
               {"yicespy_version": "f0768ffeec15ea310f830d10878971c9998454ac"}),
-    Installer(BtorInstaller,    "3.2.1", {}),
+    Installer(BtorInstaller,    "3.2.2", {}),
     Installer(PicoSATInstaller, "965",
               {"pypicosat_minor_version" : "1708010052"}),
     Installer(CuddInstaller,    "2.0.3",
