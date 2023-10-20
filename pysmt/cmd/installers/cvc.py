1df5b56c5b5a1af0ee8bfb3bfe9c54e964f43172
MikAnd <micheli.andrea@gmail.com>
2023-10-20 13:54:59 +0200

    Fixed solver version extraction for cvc5

--- pysmt/cmd/installers/cvc.py
+++ pysmt/cmd/installers/cvc.py
@@ -39,4 +39,4 @@ class CVC5Installer(SolverInstaller):
         SolverInstaller.run_python("-m pip install --target=%s cvc5==%s" % (self.bindings_dir, self.solver_version))
 
     def get_installed_version(self):
-        return self.get_installed_version_script(self.bindings_dir, "cvc")
+        return self.get_installed_version_script(self.bindings_dir, "cvc5")
