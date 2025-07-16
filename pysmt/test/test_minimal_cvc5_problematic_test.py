e7e15603e3a9f994c77d85bc6f46f34ab9b543fb
Luca Framba <framba@fbk.eu>
2025-07-16 12:00:10 +0200

    add(crash test cvc5): add a test that might always crash on cvc5

---
+++ pysmt/test/test_minimal_cvc5_problematic_test.py
@@ -0,0 +1,32 @@
+import pytest
+
+import os
+from subprocess import run
+from pysmt.shortcuts import (
+    Solver as pysmt_solver,
+    TRUE,
+    FALSE,
+    Not as pysmt_Not,
+    Symbol,
+    REAL,
+    Real,
+    Implies as pysmt_Implies,
+    Div,
+    LE,
+)
+
+
+class TestSMTSolvers:
+    def test_cvc5_strange_formula3(self):
+        s = pysmt_solver("cvc5", logic="QF_NRA", random_seed=42)
+        s.set_option("nl-ext", "none")
+        x = Symbol("x", REAL)
+        prop = pysmt_Implies(
+            (Real(20) + (Real(-1) * x)) < Real(0),
+            LE(
+                (Div(Real(187), Real(464)) * ((Real(-50) + x) * (Real(-50) + x))),
+                Div(Real(2093974), Real(5773)),
+            ),
+        )
+        result = s.is_valid(prop)
+        assert result is False, "CVC5 solver failed to handle negated linear formula 3"
