0763d5cb221136e4877df1db7485f948455fe6e2
MikAnd <micheli.andrea@gmail.com>
2025-09-05 09:59:43 +0200

    Fixed missing init parameter when probing functionality of Msat-based QE

--- pysmt/factory.py
+++ pysmt/factory.py
@@ -321,10 +321,11 @@ class Factory(object):
             from pysmt.solvers.msat import (MSatFMQuantifierEliminator,
                                             MSatLWQuantifierEliminator)
             try:
-                MSatFMQuantifierEliminator()
-                MSatLWQuantifierEliminator()
-            except:
+                MSatFMQuantifierEliminator(self.environment)
+                MSatLWQuantifierEliminator(self.environment)
+            except NotImplementedError:
                 raise SolverAPINotFound
+
             self._all_qelims['msat_fm'] = MSatFMQuantifierEliminator
             self._all_qelims['msat_lw'] = MSatLWQuantifierEliminator
         except SolverAPINotFound:
@@ -335,6 +336,12 @@ class Factory(object):
             MSATLibLoader("optimathsat")
             from pysmt.optimization.optimsat import (OptiMSATFMQuantifierEliminator,
                                                      OptiMSATLWQuantifierEliminator)
+            try:
+                MSatFMQuantifierEliminator(self.environment)
+                MSatLWQuantifierEliminator(self.environment)
+            except NotImplementedError:
+                raise SolverAPINotFound
+
             self._all_qelims['optimsat_fm'] = OptiMSATFMQuantifierEliminator
             self._all_qelims['optimsat_lw'] = OptiMSATLWQuantifierEliminator
         except SolverAPINotFound:
